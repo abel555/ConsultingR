6f82d603ee7a9df7dc07c0a92cb6c7a11fa1e3c9
abel555 <taqui555@gmail.com>
2017-08-21 16:47:23 -0400

    seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+root 'patients#index'
  resources :patients do
   	resources :reports
  end
- root 'patients#index'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
